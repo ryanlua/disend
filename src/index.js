1ec479b765267f063e0a6ed14325ed31b86ba144
Ryan Luu <ryan.luu@ryanluu.dev>
2026-03-03 06:57:48 +0000

    Use JS
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
  * - Run `npm run deploy` to publish your worker
  *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request, env, ctx) {
 		return new Response('Hello World!');
 	},
-} satisfies ExportedHandler<Env>;
+};
