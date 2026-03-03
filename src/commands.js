df5aaa2f51ee681b7db1c876e7c3721b31655ba8
Ryan Luu <ryan.luu@ryanluu.dev>
2026-03-03 07:30:33 +0000

    Add send command
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/commands.js
@@ -0,0 +1,27 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime and registration.
+ */
+
+import {
+	ApplicationCommandType,
+	ApplicationIntegrationType,
+	InteractionContextType,
+} from 'discord-api-types/v10';
+
+/**
+ * Clip command to create a clip from a message.
+ * @type {import('discord-api-types/v10').RESTPostAPIApplicationCommandsJSONBody}
+ */
+export const SEND_COMMAND = {
+	name: 'Send message',
+	description: 'Send a message to the channel',
+	integration_types: [
+		ApplicationIntegrationType.GuildInstall,
+		ApplicationIntegrationType.UserInstall,
+	],
+	contexts: [
+		InteractionContextType.PrivateChannel,
+		InteractionContextType.Guild,
+	],
+	type: ApplicationCommandType.ChatInput,
+};
